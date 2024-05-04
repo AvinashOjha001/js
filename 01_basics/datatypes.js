c3166c9cf46c2839a3fa895ccb7db9689c2a78eb
AvinashOjha001 <110490999+AvinashOjha001@users.noreply.github.com>
2024-05-04 12:48:51 +0000

    variables and datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+let name = "Avinash"
+let age = 18
+let isLoggedIn = false
+
+// number
+// bigint
+// string = ""
+// boolean
+// null => standalone value
+// undefined 
+// symbol =>unique
+// object
+
+console.log(typeof "Avinash");
+console.log(typeof null);
---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "av@google.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+
+accountEmail = "avi@gmail.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
