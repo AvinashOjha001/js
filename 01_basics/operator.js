7d9e95a22697c4cae30e09b460e84c41b5452b56
AvinashOjha001 <110490999+AvinashOjha001@users.noreply.github.com>
2024-05-04 19:08:04 +0000

    conversion

---
+++ 01_basics/operator.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNum = 33
+let stringNum = String(someNum)
+
+console.log(stringNum);
+console.log(typeof stringNum);
